3f4714f77ea56f7777f07fea135b3d3a3129f0d6
Sagar <sagarthakur6947@gmail.com>
2022-06-25 00:45:54 +0530

    database connection

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,4 @@
 <?php
 //connecting database
-// $con = mysqli_connect("localhost", "mpwalmi_test", "RO^s?a,xi+a}", "mpwalmi_test") or die("Error " . mysqli_error($con));
-$con = mysqli_connect("localhost", "root", "", "mpwalmi") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "mpwalmi_test", "RO^s?a,xi+a}", "mpwalmi_test") or die("Error " . mysqli_error($con));
+// $con = mysqli_connect("localhost", "root", "", "mpwalmi") or die("Error " . mysqli_error($con));
